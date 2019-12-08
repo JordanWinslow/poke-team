72ccea08e114d92e631c9e96f73b411054aa486e
JordanWinslow <admin@jordanwinslow.me>
2019-12-08 15:55:15 -0800

    Large & Medium Screen Component Finished

--- src/components/ListFavoritePokemon.jsx
+++ src/components/ListFavoritePokemon.jsx
@@ -1,28 +1,109 @@
-import React from "react"
-import { useSelector } from "react-redux"
+import React, { useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+
+import {
+  Typography,
+  Avatar,
+  Card,
+  CardHeader,
+  IconButton,
+  Grid,
+  Box
+} from "@material-ui/core"
+
+import CloseIcon from "@material-ui/icons/Close"
+
+import FavoriteButton from "../components/FavoriteButton"
+
+import { removeFavoritePokemon } from "../redux/features/pokeTeam/pokeTeamSlice"
+
+import capitalizeFirstLetter from "../util/capitalizeFirstLetter"
+
 const ListFavoritePokemon = ({ history }) => {
+  const dispatch = useDispatch()
   const { favoritePokemon } = useSelector(state => state.pokeTeam)
+  const { pokemon } = useSelector(state => state.pokeAPI)
+  const getAvatar = name => {
+    return pokemon.find(p => p.name === name).sprites.front_default
+  }
+  const getTypes = name => {
+    return pokemon.find(p => p.name === name).types
+  }
   return (
-    <div
+    <Box
       style={{
-        backgroundColor: "white",
-        zIndex: "3",
-        width: "50%",
-        margin: "auto",
-        borderRadius: "3px",
-        boxShadow: "0 3px 3px 3px rgba(0, 0, 0, 0.2)",
-        padding: "3rem"
+        width: "100vw",
+        height: "100vh",
+        position: "fixed",
+        top: 0,
+        zIndex: 9999,
+        backdropFilter: "blur(5px)",
+        display: "flex",
+        justifyContent: "center"
       }}
     >
-      <button onClick={() => history.goBack()}>CLOSE</button>
-      <h1>work in progress</h1>
-      <h2>Your Favorited Pokemon:</h2>
-      <div>
-        {favoritePokemon.map(name => (
-          <h3>{name}</h3>
-        ))}
-      </div>
-    </div>
+      <Grid
+        id="FavoritesList"
+        container
+        sm={12}
+        md={8}
+        lg={6}
+        style={{ overflowY: "scroll", margin: "3rem 0" }}
+      >
+        <Card>
+          <CardHeader
+            action={
+              <IconButton aria-label="close favorites menu">
+                <CloseIcon onClick={() => history.goBack()} />
+              </IconButton>
+            }
+            title="Favorite Pokemon"
+          />
+          <Grid container>
+            {favoritePokemon.map(name => (
+              <Grid
+                item
+                sm={12}
+                md={6}
+                style={{
+                  padding: "1rem",
+                  height: "6rem",
+                  display: "flex",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                  boxShadow: "0 0 1px rgba(0, 0, 0, 0.5)"
+                }}
+              >
+                <Avatar
+                  style={{
+                    height: "4rem",
+                    width: "4rem",
+                    cursor: "pointer"
+                  }}
+                  src={getAvatar(name)}
+                  className="fullSizeAvatar" // index.css
+                  alt={name}
+                  onClick={() => history.push("/pokemon/" + name)}
+                ></Avatar>
+                <Typography
+                  variant="h6"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => history.push("/pokemon/" + name)}
+                >
+                  {capitalizeFirstLetter(name)}
+                </Typography>
+                {/*secondary={/*types*/}
+                <FavoriteButton
+                  isFavorited={true}
+                  onClick={() => dispatch(removeFavoritePokemon(name))}
+                  aria-label="remove pokemon from favorites"
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Card>
+      </Grid>
+    </Box>
   )
 }
 
